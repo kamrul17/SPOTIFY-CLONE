6edf4f1dc0e7d20c767a7377e3ad3b508f9d8069
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2024-01-17 20:33:50 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-console.log("let do some");
 let currentSong = new Audio();
+const playButton = document.getElementById("play");
+let songs;
+let previous = document.querySelector("#previous");
+let next = document.querySelector("#next");
+
+function secondsToMinutesAndSeconds(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  // Use template literals to format the output with leading zeros
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
   let response = await a.text();
@@ -23,22 +38,30 @@ async function getSongs() {
 }
 
 // play music func
-const playMusic = (track) => {
+const playMusic = (track, pause = false) => {
   currentSong.src = "/songs/" + track;
-  currentSong.play();
+  if (!pause) {
+    currentSong.play();
+    playButton.classList.remove("fa-play");
+    playButton.classList.add("fa-pause");
+  }
+
+  document.querySelector(".songInfo").innerHTML = decodeURI(track);
+  // document.querySelector(".songTime").innerHTML = "00:00 / 00:00";
 };
 async function main() {
-  let currentSong;
-  let songs = await getSongs();
-  //   console.log(songs);
+  songs = await getSongs();
+  //   play first song bydefault
+  playMusic(songs[0], true);
   let songul = document
     .querySelector(".songlists")
     .getElementsByTagName("ul")[0];
+
   for (const song of songs) {
     songul.innerHTML += `<li><div class="info">
-    <div> ${song.replaceAll("%20", " ")}</div>
-   
-</div></li>`;
+      <div> ${song.replaceAll("%20", " ")}</div>
+
+  </div></li>`;
   }
   Array.from(
     document.querySelector(".songlists").getElementsByTagName("li")
@@ -48,9 +71,68 @@ async function main() {
       playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
     });
   });
+
+  // playbtn eventlistenner
+
+  playButton.addEventListener("click", (e) => {
+    if (currentSong.paused) {
+      currentSong.play();
+      playButton.classList.remove("fa-play");
+      playButton.classList.add("fa-pause");
+    } else {
+      currentSong.pause();
+      playButton.classList.remove("fa-pause");
+      playButton.classList.add("fa-play");
+    }
+  });
+
+  // listen for timeupdate and also seekbar
+  currentSong.addEventListener("timeupdate", () => {
+    // console.log(currentSong.currentTime, currentSong.duration);
+    document.querySelector(
+      ".songTime"
+    ).innerHTML = `${secondsToMinutesAndSeconds(currentSong.currentTime)} /
+      ${secondsToMinutesAndSeconds(currentSong.duration)}
+    `;
+    document.querySelector(".circle").style.left =
+      (currentSong.currentTime / currentSong.duration) * 100 + "%";
+  });
+
+  // Add an event listener to seekbar
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = (currentSong.duration * percent) / 100;
+  });
+
+  // Add an event listener to previous
+  previous.addEventListener("click", () => {
+    currentSong.pause();
+    console.log("Previous clicked");
+    let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+    if (index - 1 >= 0) {
+      playMusic(songs[index - 1]);
+    }
+  });
+
+  previous.addEventListener("click", () => {
+    currentSong.pause();
+    console.log("Previous clicked");
+    let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+    if (index - 1 >= 0) {
+      playMusic(songs[index - 1]);
+    }
+  });
+
+  // Add an event listener to next
+  next.addEventListener("click", () => {
+    currentSong.pause();
+    console.log("Next clicked");
+
+    let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+    if (index + 1 < songs.length) {
+      playMusic(songs[index + 1]);
+    }
+  });
 }
 main();
-// songul.innerHTML += `<li>${song.replaceAll(
-//   "%20",
-//   ""
-// )}<span style="padding-left:3px;"><i class="fa-solid fa-music"></i></span></li>
